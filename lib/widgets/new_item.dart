64ddc817e1d284a82445ee43ca9b4550ffab9949
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2023-09-12 19:06:17 +0530

    NewItem Screen Installed

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a new Item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Text('the form'),
+      ),
+    );
+  }
+}
